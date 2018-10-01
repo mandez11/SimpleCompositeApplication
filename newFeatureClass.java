c8c6bab70a84c33938b337b0461986d6ea7751eb
mandez11 <dr.mandez@gmail.com>
2018-10-01 17:15:18 +0300

    init commit to 'new_feature'

---
+++ newFeatureClass.java
@@ -0,0 +1,12 @@
+public class NewFeatureClass
+{
+	public String newFeatureMethod1()
+	{}
+	
+	public String newFeatureMethod2()
+	{}
+
+	/* adding super method to new_feature */
+	public String newFeatureSuperMethod()
+	{}
+}
