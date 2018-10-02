0829880538f700eaf8f3f7e4b8691473fb39d328
mandez11 <dr.mandez@gmail.com>
2018-10-02 10:33:23 +0300

    add new 'cool_feature'

---
+++ coolFeatureClass.java
@@ -0,0 +1,31 @@
+public class CoolFeatureClass
+{
+    private int intField;
+    private string strField;
+
+    public CoolFeatureClass()
+    {
+    }
+
+    /* Getters/Setters Methods */
+
+    public int getIntField()
+    {
+        return intField;
+    }
+
+    public void setIntField(int value)
+    {
+        intField = value;
+    }
+
+    public int getStrField()
+    {
+        return strField;
+    }
+
+    public void setStrField(string value)
+    {
+        strField = value;
+    }
+}
