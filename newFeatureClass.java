f19cb57651a05d3d95a2c4ad8fdf92933d8dfe19
mandez11 <dr.mandez@gmail.com>
2018-10-02 09:17:09 +0300

    little changes

---
+++ newFeatureClass.java
@@ -0,0 +1,14 @@
+public class NewFeatureClass
+{
+	public String newFeatureMethod1()
+	{}
+	
+	public String newFeatureMethod2()
+	{}
+
+	/* adding super method to new_feature */
+	public String newFeatureSuperMethod()
+	{}
+	
+
+}
