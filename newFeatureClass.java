569b9bc987a936aea04dae7df8bcfea213b483c5
mandez11 <dr.mandez@gmail.com>
2018-10-26 11:34:30 +0300

    new branch 'dev_test2'

---
+++ newFeatureClass.java
@@ -0,0 +1,38 @@
+public class NewFeatureClass
+{
+	public String newFeatureMethod1()
+	{}
+	
+	public String newFeatureMethod2()
+	{}
+
+	/* adding super method to new_feature */
+	public String newFeatureSuperMethod()
+	{
+		
+		// Developer #1 add some action to method
+		// ...
+		
+		// Developer #2 more improve
+		// ...
+
+		// Add from 'release/2.0.1' branch
+		// ...
+
+		// Add from 'test' branch
+		// ...
+
+		// Add from 'test2' branch
+		// ...
+
+		// Add from 'dev_test2' branch
+		// ...
+		
+	}
+
+	/* GetIntValue */
+	public int GetIntValue()
+	{
+		return 10;
+	}
+}
