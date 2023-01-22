d556b268a6bbe1c3c5498c61b524253293557af3
Vladyslav Mysiura <dr.mandez@gmail.com>
2023-01-22 11:38:44 +0200

    adding Backend.java file

---
+++ Backend.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+import java.util.*;
+import java.io.*;  
+
+
+/**
+ * Backend
+ */
+public class Backend {
+    public void run(String ip, int port) {
+        // TODO: starting the backend server
+    } 
+    
+    public void stop() {
+        // TODO: stopping the backend execution
+    }
+}
