820a850123d131b35105ad07e3532870d8e66179
Vladyslav Mysiura <dr.mandez@gmail.com>
2023-01-22 11:53:37 +0200

    frontend: extending Frontend

--- Frontend.java
+++ Frontend.java
@@ -5,5 +5,10 @@ import java.io.*;
 public class Frontend {
     public void doSomeFrontendStuff() {
         // TODO: doing some frontend stuff.
+        this.anotherFrontendMethod(); // calling new method
+    }
+
+    public void anotherFrontendMethod() {
+        // some code here...
     }
 }
